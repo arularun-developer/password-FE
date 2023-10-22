45de614e1499f85c4a23e5ac623f24ba30e07508
arularun-developer <arularunoffical1110@gmail.com>
2023-10-22 16:16:40 +0530

    first commit

--- src/components/Resetpassword.jsx
+++ src/components/Resetpassword.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
+ import { useNavigation } from 'react-router-dom';
+
 
 function Resetpassword() {
+  const navigate = useNavigation("")
   const [confirmpassword, setconfirmpassword] = useState("")
 
 
@@ -10,39 +13,41 @@ function Resetpassword() {
   const token = window.location.pathname.split("/").pop();
   const handlesumbitpassword = async (e) => {
     e.preventDefault();
-  
-      // Basic validation: Check if the password is not empty.
-      // if (!password) {
-      //   console.log("Password is required");
-      //   return; // Exit the function to prevent the API request when the password is empty.
-      // }
-    
-      try {
-        const response = await axios.post(
-          `https://password-reset-gd3w.onrender.com/user/reset-password/${token}`,
-          {
-            newPassword: confirmpassword,
-          }
-        );
-    
-        if (response.status === 200) {
-         toast.success("Password reset successful");
-         setconfirmpassword("")
-          // You can redirect the user to a success page or perform other actions.
+
+    // Basic validation: Check if the password is not empty.
+    // if (!password) {
+    //   console.log("Password is required");
+    //   return; // Exit the function to prevent the API request when the password is empty.
+    // }
+
+    try {
+      const response = await axios.post(
+        `https://password-reset-gd3w.onrender.com/user/reset-password/${token}`,
+        {
+          newPassword: confirmpassword,
         }
-      } catch (error) {
-        console.error("Error:", error);
-        // Handle the error, display an error message, or redirect the user to an error page.
+      );
+
+      if (response.status === 200) {
+        toast.success("Password reset successful");
+
+        setconfirmpassword("")
+        setTimeout(()=>{navigate('/login')},5000);
+        // You can redirect the user to a success page or perform other actions.
       }
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle the error, display an error message, or redirect the user to an error page.
+    }
 
   }
   return (
     <form onSubmit={handlesumbitpassword}
-   
+
 
       className="signIn container d-flex flex-column gap-3 p-2"
     >
-       <ToastContainer />
+      <ToastContainer />
       <h2 className="display-6 text-light text-center ">New Password</h2>
       <div className="form-group d-flex flex-column gap-1">
 
@@ -57,10 +62,12 @@ function Resetpassword() {
 
         />
       </div>
+     
 
       <button type="submit" className="btn btn-primary">
         Sumbit
       </button>
+
     </form>
   )
 }
